feb36589bd722eddf88d6687cd7607680cfe3593
youki0607 <its_showtime@i.softbank.jp>
2020-10-04 19:58:06 +0900

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "blocks#index"
+  root to: "blocks#index"
 end
